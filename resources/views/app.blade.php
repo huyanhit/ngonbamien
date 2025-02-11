10fcc877376c146d8cfbb5d735866894f0d5371d
Huy Le <huyanhit@gmail.com>
2025-02-11 15:31:53 +0700

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="zxx">
-
+<html lang="vi">
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Ogani Template">
@@ -757,16 +756,11 @@
 
     <!-- Js Plugins -->
     <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.nice-select.min.js"></script>
     <script src="js/jquery-ui.min.js"></script>
     <script src="js/jquery.slicknav.js"></script>
     <script src="js/mixitup.min.js"></script>
     <script src="js/owl.carousel.min.js"></script>
-    <script src="js/main.js"></script>
-
-
-
 </body>
 
 </html>
